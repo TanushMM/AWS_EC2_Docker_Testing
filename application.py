c51b7126de3ec4c7d02a6fad22c8b7c76718771b
Tanush <tanushmmofficial@gmail.com>
2024-08-12 20:48:18 +0530

    Updated application.py

--- application.py
+++ application.py
@@ -1,10 +1,11 @@
 from flask import Flask 
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route("/")
+@application.route("/")
 def main():
     return "<h1>Hello World from AWS EC2 via Docker</h1>"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@application.route("/hello")
+def hello():
+    return "<h1>Hello from '/hello' </h1>"
